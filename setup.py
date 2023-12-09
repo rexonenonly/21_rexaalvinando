2fef7da0856842bc0adc1a9b371b3ac331e2bdff
Rex <successfull1168@gmail.com>
2023-12-09 13:42:56 +0700

    commit kedua

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paket_js_13',
+    version='0.1.0',
+    author='Rexa Alvinando',
+    author_email='successfull168@gmail.com',
+    description='Hello Its me, Rex',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
